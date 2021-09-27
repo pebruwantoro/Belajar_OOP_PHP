396cc32523ce6effaac1be53a6ff17dd4a46854b
Doni Pebruwantoro <pebruwantoro.doni@gmail.com>
2021-09-27 11:08:59 +0700

    add constructor overriding

--- data/Manager.php
+++ data/Manager.php
@@ -4,7 +4,14 @@ class Manager
 {
 
 	var string $name;
+	var string $title;
 	
+	public function __construct(string $name ="", string $title ="Manager")
+	{
+		$this->name = $name;
+		$this->title = $title;
+	}
+
 	function say_hello(string $name)
 	{
 		echo "Hi $name, my name is manager $this->name" .PHP_EOL;
@@ -13,6 +20,12 @@ class Manager
 
 class ViceManager extends Manager 
 {
+	public function __construct(string $name ="")
+	{
+		// tidak wajib tapi direkomendasikan
+		parent::__construct($name, "VP");
+	}
+
 	function say_hello(string $name)
 	{
 		echo "Assalamualaikum $name, nami kulo $this->name" .PHP_EOL;
